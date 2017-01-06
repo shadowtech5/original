4c3f61f0a8009d5e0c290f8eb148da823e266266
shadowtech5 <shadowtechlt@gmail.com>
2017-01-06 12:11:56 +0530

    added 404

--- 404.php
+++ 404.php
@@ -6,14 +6,14 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/404-style.css">
 		<link href='http://fonts.googleapis.com/css?family=Condiment' rel='stylesheet' type='text/css'>
 </head>
 <body>
 <div class="wrap">
 	<div class="main">
 		<div class="banner">
-			<img src="images/banner.png" alt="" />
+			<img src="img/banner.png" alt="" />
 		</div>
 		<div class="text">
 			<h1>The requested url was not found !...</h1>
