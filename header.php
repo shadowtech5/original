6760e3b7534834a90e0e2b60de0c8f3f3d5d89b1
Jishnunand <jishnunand.kozhisseri@tarams.com>
2017-01-29 20:53:37 +0530

    added header changes

--- header.php
+++ header.php
@@ -1,39 +1,25 @@
-<div class="container-fluid light-white social-container">
-	<div class="col-md-4 logo">
-		<a href="index.php">
-			<img src="img/logo.png" class="img-responsive">
-		</a>
-	</div>
-	<div class="col-md-4 col-sm-offset-2 Search-box">
-		<form action="landingpage.php?search" method="post">
-		<div class="input-group stylish-input-group">
-	            <input type="text" class="form-control"  placeholder="Search with name or bma code" name="bma_code">
-		        <span class="input-group-addon">
-	                <button type="submit">
-						<span class="glyphicon glyphicon-search"></span>
-                    </button>  
-                </span>
-        </div>
-        </form>
-	</div>
-	<div class="menu-list">
-		<span><a href="">OFFERS</a></span>
-		<span><a href="">GIFITING</a></span>
-		<span class="">
-		<?php
-		if(!isset($_COOKIE['user_email']))
-		{
-		?>
-			<span class="glyphicon glyphicon-user"></span><a class="sign_button">SIGN IN</a>
-		<?php
-		}
-		else
-		{
-		?>
-		<span class="glyphicon glyphicon-user"></span>
-		<span></span>
-		<ul class="profile_details">
-			<li class="dropdown">
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+	    <div class="navbar-header col-md-2">
+			<a href="index.php">
+				<img src="img/logo.jpg" class="img-responsive">
+			</a>
+	    </div>
+      	<ul class="nav navbar-nav navbar-right">
+	        <li>
+	        	<?php
+					if(!isset($_COOKIE['user_email']))
+					{
+				?>
+	          	<a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+	          	<ul class="dropdown-menu">
+		        	<li><a href="login_social_google.php?provider=google"><span class="fa fa-google-plus fa-lg"></span> log in with Google</a></li>
+		        </ul>
+	          	<?php
+					}
+					else
+					{
+				?>
 				<a class="dropdown-toggle" data-toggle="dropdown" href="#">
 					<?php 
 						include 'db_connnection.php';
@@ -50,30 +36,15 @@
 					?>
 					<span class="caret"></span>
 				</a>
-				<ul class="dropdown-menu">
-          			<li><a href="#">Dashboard</a></li>
+		        <ul class="dropdown-menu">
+		        	<li><a href="#">Dashboard</a></li>
           			<li><a href="#">Profile</a></li>
           			<li><a href="logout.php">Logout</a></li>
-          		</ul>
-          	</li>
-        </ul>
-		</span>
-		<?php
-		}
-		?>
-	</div>
-	<!-- login box -->
-		<div class="login-box text-center container-fluid col-md-6 right">
-			<div class="social-login col-md-12 text-center">
-					<!-- <a href="#">
-			        <i class="fa fa-facebook fa-lg"></i>
-			        Login in with facebook
-					</a> -->
-					<a href="login_social_google.php?provider=google">
-					<i class="fa fa-google-plus fa-lg"></i>
-					log in with Google
-					</a>
-			</div>
-		</div>
-	<!-- end -->
-</div>
+		        </ul>
+		        <?php
+		        	}
+		        ?>
+	        </li>
+      	</ul>
+    </div>
+</nav>
